2b9b2e22875ce0bc91fd907a668f4fddedf20649
CozyCloud22 <DCbankai888@gmail.com>
2023-04-18 19:40:28 -0400

    final update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import csv
 import random
+import pandas as pd
+import urllib
 
+#defining global variables and dictionaries
 currency = 0
 rod = "Bamboo Rod"
 
@@ -40,26 +43,29 @@ def fish(rod):
     
     #routing the CSV files to the code
     allFiles = {
-        "Bamboo Rod": "C:\\Users\\dcnat\\OneDrive\\Desktop\\fishy-game\\data\\BambooRod.csv",
-        "Stone Rod": "C:\\Users\\dcnat\\OneDrive\\Desktop\\fishy-game\\data\\StoneRod.csv",
-        "Metal Rod": "C:\\Users\\dcnat\\OneDrive\\Desktop\\fishy-game\\data\\MetalRod.csv",
+        "Bamboo Rod": "https://raw.githubusercontent.com/CozyCloud22/fishy-game/main/data/BambooRod.csv",
+        "Stone Rod": "https://raw.githubusercontent.com/CozyCloud22/fishy-game/main/data/StoneRod.csv",
+        "Metal Rod": "https://raw.githubusercontent.com/CozyCloud22/fishy-game/main/data/MetalRod.csv"
     }
-    
-    #opens the CSV files and adds them to a list
-    with open(allFiles[rod], newline = '') as csvfile:
-        fishreader = csv.reader(csvfile)
-        bigList = [fish.strip() for row in fishreader for fish in row]
+    url = allFiles[rod]
 
-    #chooses a random fish
+    #read the specified CSV file
+    df = pd.read_csv(url)
+
+    #extract the values from the DataFrame and add them to a list
+    bigList = df.values.flatten().tolist()
+
     randomFish = random.choice(bigList)
 
+    randomFish = randomFish.strip()
+    
     return randomFish
 
 def inventory():
     fishHolder = []
 
     #self note. f" is a string literal {} <-- contains the string. Very useful
-    print(f"You have: {currency} dollars and these fishies: ")
+    print(f"You have: {currency} dollar, a {rod}, and these fishies: ")
     
     #for loop to iterate the count based on the number of fishies. Iterates over every item in dictionary
     for fishies, count in fishInventory.items():
@@ -75,14 +81,18 @@ def inventory():
 #function that updates the global dictionary of fish inventory
 def updateFishCount(fishName):
     global fishInventory
+    
+    #updates the global dictionary fish count if fish is caught
     if fishName in fishInventory:
         fishInventory[fishName] += 1
 
+#function for store transactions
 def storeContent():
     print("Hello and welcome to Cloudy's fish shop where you can sell your fish and buy fishing rods!")
-    #create a messagepost sell fish
 
+    #while loop to continue user shopping until user wants to exit
     while(True):
+        #defining global variables that will be used
         global currency
         global rod
 
@@ -91,34 +101,47 @@ def storeContent():
             totalValue = 0
             secondQuestion = input("\nWhat would you like to sell (You can click enter 'all' as well): ")
             
+            #checks to see if the fish is a valid one
             if(secondQuestion in fishInventory):
                 thirdQuestion = int(input("\nHow many would you like to sell? "))
+                
+                #checks to see if user has the correct amount
                 if(thirdQuestion <= fishInventory[secondQuestion]):
                     tempVal = fishPrices[secondQuestion]
                     totalValue += tempVal
                     fishInventory[secondQuestion] -= thirdQuestion
+                    print("Successfully sold!")
                 else:
                     print("You don't have enough fishies")
                 
                 currency += totalValue
+
+            #gives the user the option to sell all
             elif(secondQuestion == "all"):
                 allZeroes = True
+
+                #iterates through all the possible fish the user has to sell them
                 for temp, count in fishInventory.items():
                     if(count > 0):
                         tempVal = fishPrices[temp]
                         totalValue += tempVal
                         fishInventory[temp] = 0
+                        
                     if(count != 0):
                         allZeroes = False
+                
+                #sends a message to user that they have nothing in inventory
                 if(allZeroes == True):
                     print("You have nothing to sell :(")
-
+                else:
+                    print("Successfully sold!")
 
                 currency += totalValue
             elif(secondQuestion == "exit"):
                 break
             else:
                 print("That's not a valid response, please try again.")
+        #sets the user up to buy a fishing rod
         elif(question == "buy"):
             storage = ""
             storageList = []
@@ -126,6 +149,8 @@ def storeContent():
                 storageList.append(f"{rods}: {prices}")
             storage = "\n".join(storageList)
             print(f"You can currently buy: \n{storage}")
+            
+            #user input to buy rod
             questionFour = input("\nWhat would you like to buy?('exit' to leave) ")
             if(questionFour in rodPrices):
                 if(currency >= rodPrices[questionFour]):
@@ -137,6 +162,7 @@ def storeContent():
             else:
                 print("That is not a valid response")
             
+        #allows user to leave
         elif(question == "exit"):
             return ""
         else:
@@ -158,21 +184,15 @@ def main():
                 tempVar = fish(rod)
                 print(f"\nYou got: {tempVar}!")
                 updateFishCount(tempVar)
-            
             elif(userInput == "inventory"):
                 print()
                 print(inventory())
             elif(userInput == "store"):
-                #make a function
                 print(storeContent())
-            elif(userInput == "achievements"):
-                #make an achievements list
-                print("achievements")
             elif(userInput == "exit"):
                 gameContinue = False;
             else:
                 print("That was an invalid response. Please try again.")
             
-
 if __name__ == "__main__":
     main()
